288560c89f8645d0029a801610b4b288887aabd4
Matheus Aguiar <aguiartgv@gmail.com>
2023-10-18 20:06:17 -0300

    feat: creating template for the sing in page

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,3 +9,11 @@ module.exports = {
   },
   plugins: [],
 }
+/**
+ 'bg-gymm-dark-100': '#ffc68a',
+    'bg-gymm-orange-100': '#ffc68a',
+    'bg-gymm-orange-200': '#fead73',
+    'bg-gymm-orange-300': '#fe945c',
+    'bg-gymm-orange-400': '#fd7a45',
+    'bg-gymm-orange': '#fc612e',
+ **/
